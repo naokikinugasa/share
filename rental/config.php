a1cf1e2ab22fd0a5cfc668dfb4b2e5bd7fb4f00c
naokikinugasa <s1410568@u.tsukuba.ac.jp>
2018-01-24 21:41:57 +0900

    設定

--- rental/config.php
+++ rental/config.php
@@ -2,9 +2,14 @@
 
 
 ini_set('display_errors', 1);
-
+/*
 define('DSN', 'mysql:host=mysql5.minibird.netowl.jp;dbname=newto_rental');
 define('DB_USERNAME', 'newto_naoki');
+define('DB_PASSWORD', 'vrsc7572');
+*/
+
+define('DSN', 'mysql:host=localhost:8889;dbname=share');
+define('DB_USERNAME', 'naoki');
 define('DB_PASSWORD', 'vrsc7572');
 
 
